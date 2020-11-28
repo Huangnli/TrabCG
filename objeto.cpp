b855ebaa0a17f6601989c571e7962a29978361f2
Huangnli <50835276+Huangnli@users.noreply.github.com>
2020-11-28 09:50:08 -0400

    removi ler file.obj para app.cpp -atualizei as informacoes que objeto recebe

--- objeto.cpp
+++ objeto.cpp
@@ -1,104 +1,119 @@
-
-#include <vector>
-#include <cstdio>
-#include <glm/fwd.hpp>
-#include <glm/vec3.hpp>
 #include "objeto.h"
 #include "string.h"
 
-
+#define GL_GLEXT_PROTOTYPES 1
+#define GL3_PROTOTYPES 1
 #define MAX 50
 
 using namespace std;
 
-objeto::objeto(const char *path){
-    
-        //temporary variables to store the .obj's contents:
-        vector<unsigned int> vertexIndices, normalIndices;
-        vector<glm::vec3> temp_vertices;
-        vector<glm::vec3> temp_normals;
+objeto::objeto(string name, unsigned int idvao, unsigned int idvbo , vector<glm::vec3> dadosDoObjeto, glm::mat4 matrizmodel, 
+            glm::mat4 matrizview, glm::mat4 matrizprojection, glm::vec3 cor)
+{
+    nome = name;
+    vao = idvao;
+    vbo = idvbo;
+    vertexBuffer = dadosDoObjeto;
+    model = matrizmodel;
+    view = matrizview;
+    projection = matrizprojection;
+    outColor = cor;
 
-        //reading the file data
-        FILE *file = fopen(path, "r");
-        char line[MAX];
-
-        if (file == NULL){
-            printf("erro ao abrir o arquivo!\n");
-        }
-        int res = fscanf(file, "%s", line); //read the first word
-        while (res != EOF){
-
-            if (strcmp(line, "v") == 0){ //verify is it's a vertex position
-
-                glm::vec3 vertex;
-                fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z); //copy the coordinates to auxiliar vertex
-                temp_vertices.push_back(vertex);                             //copy the coordinates to the temporary vecVertex
-            }
-            else if (strcmp(line, "vn") == 0){ //verify if it's a normal data
-                glm::vec3 normal;
-                fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
-                temp_normals.push_back(normal);
-            }
-
-            else if (strcmp(line, "f") == 0)
-            { //verify if it's a face, and make a index contents
-                unsigned int vertexIndex[3], textureIndex[3], normalIndex[3];
-                int matches = fscanf(file, "%d/%d/%d  %d/%d/%d  %d/%d/%d\n", &vertexIndex[0], &textureIndex[0], &normalIndex[0],
-                                                                            &vertexIndex[1], &textureIndex[0], &normalIndex[1],
-                                                                            &vertexIndex[2], &textureIndex[0], &normalIndex[2]);
-                if (matches != 9){
-                    printf("error in reading file! try another way.\n\n");
-                    
-                }
-                //indexação dos vertices e normais
-                vertexIndices.push_back(vertexIndex[0]);
-                vertexIndices.push_back(vertexIndex[1]);
-                vertexIndices.push_back(vertexIndex[2]);
-
-                normalIndices.push_back(normalIndex[0]);
-                normalIndices.push_back(normalIndex[1]);
-                normalIndices.push_back(normalIndex[2]);
-            }
-            res = fscanf(file, "%s", line); //continue reading
-        }
-        fclose(file);
-
-        //passing by each vertex (v/vn) of each triangule (face)
-        //put the right vertex positions and normals on the out_vertexData
-        for (unsigned int i = 0; i < vertexIndices.size(); i++)    {
-
-            unsigned int indexV = vertexIndices[i];
-            unsigned int indexN = normalIndices[i];
-
-            glm::vec3 vertex = temp_vertices[indexV - 1];
-            glm::vec3 normal = temp_normals[indexN - 1];
-            vbuffer.push_back(vertex);
-            vbuffer.push_back(normal);
-        }
-
-        model = glm::mat4(1.0);
-        nVertices = vbuffer.size()/2;
-
-        //vertexBuffer aux(vbuffer.data(), vbuffer.size() * sizeof(glm::vec3));
-        //vbo = aux.getVBOID();
 }
 
 objeto::~objeto(){
 
 }
 
-unsigned int objeto::getVBO(){
-    return vbo;
-}
+// void objeto::loadObj(const char *path){
+//     //temporary variables to store the .obj's contents:
+//         vector<unsigned int> vertexIndices, normalIndices;
+//         vector<glm::vec3> temp_vertices;
+//         vector<glm::vec3> temp_normals;
 
-int objeto::getNumeroVertices(){
-    return nVertices;
-}
+//         //reading the file data
+//         FILE *file = fopen(path, "r");
+//         char line[MAX];
 
-vector<glm::vec3> objeto::getVertexBuffer(){
-    return vbuffer; 
+//         if (file == NULL){
+//             printf("erro ao abrir o arquivo!\n");
+//         }
+//         int res = fscanf(file, "%s", line); //read the first word
+//         while (res != EOF){
 
-}
-glm::mat4 objeto::getModel(){
-    return model;
-}
+//             if (strcmp(line, "v") == 0){ //verify is it's a vertex position
+
+//                 glm::vec3 vertex;
+//                 fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z); //copy the coordinates to auxiliar vertex
+//                 temp_vertices.push_back(vertex);                             //copy the coordinates to the temporary vecVertex
+//             }
+//             else if (strcmp(line, "vn") == 0){ //verify if it's a normal data
+//                 glm::vec3 normal;
+//                 fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+//                 temp_normals.push_back(normal);
+//             }
+
+//             else if (strcmp(line, "f") == 0)
+//             { //verify if it's a face, and make a index contents
+//                 unsigned int vertexIndex[3], textureIndex[3], normalIndex[3];
+//                 int matches = fscanf(file, "%d/%d/%d  %d/%d/%d  %d/%d/%d\n", &vertexIndex[0], &textureIndex[0], &normalIndex[0],
+//                                                                             &vertexIndex[1], &textureIndex[0], &normalIndex[1],
+//                                                                             &vertexIndex[2], &textureIndex[0], &normalIndex[2]);
+//                 if (matches != 9){
+//                     printf("error in reading file! try another way.\n\n");
+                    
+//                 }
+//                 //indexação dos vertices e normais
+//                 vertexIndices.push_back(vertexIndex[0]);
+//                 vertexIndices.push_back(vertexIndex[1]);
+//                 vertexIndices.push_back(vertexIndex[2]);
+
+//                 normalIndices.push_back(normalIndex[0]);
+//                 normalIndices.push_back(normalIndex[1]);
+//                 normalIndices.push_back(normalIndex[2]);
+//             }
+//             res = fscanf(file, "%s", line); //continue reading
+//         }
+//         fclose(file);
+
+//         //passing by each vertex (v/vn) of each triangule (face)
+//         //put the right vertex positions and normals on the out_vertexData
+//         for (unsigned int i = 0; i < vertexIndices.size(); i++)    {
+
+//             unsigned int indexV = vertexIndices[i];
+//             unsigned int indexN = normalIndices[i];
+
+//             glm::vec3 vertex = temp_vertices[indexV - 1];
+//             glm::vec3 normal = temp_normals[indexN - 1];
+//             vbuffer.push_back(vertex);
+//             vbuffer.push_back(normal);
+//         }
+// }
+
+// unsigned int objeto::getVBO(){
+//     return vbo;
+// }
+
+// int objeto::getNumeroVertices(){
+//     return nVertices;
+// }
+
+// vector<glm::vec3> objeto::getVertexBuffer(){
+//     return vbuffer; 
+// }
+
+// unsigned int objeto::getVAO(){
+//     return vao;
+// }
+
+// unsigned int objeto::getVBO(){
+//     return vbo;
+// }
+
+// unsigned int getprogramId(){
+//     return programId;
+// }
+
+// glm::mat4 objeto::getModel(){
+//     return model;
+// }
--- objeto.h
+++ objeto.h
@@ -1,5 +1,6 @@
 #include <vector>
 #include <cstdio>
+#include <string>
 #include <glm/fwd.hpp>
 #include <glm/vec3.hpp>
 #include <glm/mat4x4.hpp>
@@ -8,22 +9,18 @@ using namespace std;
 
 class objeto
 {
-private:
-    vector<glm::vec3> vertexPositions, vertexNormals;
-    vector<glm::vec3> vbuffer;
-    glm::mat4 model;
-
-    int nVertices;
-    unsigned int vbo;
-
 public:
-    objeto(const char *path);
+    objeto(string name, unsigned int idvao, unsigned int idvbo , vector<glm::vec3> dadosDoObjeto, glm::mat4 matrizmodel, 
+            glm::mat4 matrizview, glm::mat4 matrizprojection, glm::vec3 cor);
     ~objeto();
 
-    vector<glm::vec3> getVertexBuffer();
-    glm::mat4 getModel();
-    unsigned int getVBO();
-    int getNumeroVertices();
+    string nome;
+    unsigned int vao;
+    unsigned int vbo;
+    vector<glm::vec3> vertexBuffer;
+    glm::mat4 model;
+    glm::mat4 view;
+    glm::mat4 projection;
+    glm::vec3 outColor;
 
-    bool loadObj(const char *path);
 };
