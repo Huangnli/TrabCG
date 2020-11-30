3a3ce50b34652e71d7cd2ac270246f4525813923
amandavc <amandavc27@gmail.com>
2020-11-30 18:50:55 -0400

    add camera class

---
+++ camera.h
@@ -0,0 +1,57 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
+
+class camera{
+    //private:
+    
+    public:
+        // camera Attributes
+        glm::vec3 position;
+        glm::vec3 lookat;
+        glm::vec3 viewup;
+   
+        camera();
+        ~camera();
+        glm::vec3 getInitialPosition();
+        glm::vec3 getInitialLookat();
+        glm::vec3 getInitialUp();
+        glm::mat4 getViewMatrix();
+
+        void setLookat(glm::vec3 vecLookAt);
+        void setPosition(glm::vec3 vecPos);
+};
+
+camera::camera(){
+    position = getInitialPosition();
+    lookat = getInitialLookat();
+    viewup = getInitialUp();
+}
+
+camera::~camera(){
+}
+
+glm::vec3 camera::getInitialPosition(){
+    return glm::vec3(0.0f, 0.0f, 0.0f);
+}
+
+glm::vec3 camera::getInitialLookat(){
+    return glm::vec3(0.0f, 0.0f, -1.0f);
+}
+
+glm::vec3 camera::getInitialUp(){
+    return glm::vec3(0.0f, 1.0f, 0.0f);
+}
+
+glm::mat4 camera::getViewMatrix(){
+    return glm::lookAt(position, lookat, viewup);
+}
+
+void camera::setLookat(glm::vec3 vecLookAt){
+    lookat = vecLookAt;
+}
+
+void camera::setPosition(glm::vec3 vecPos){
+    position = vecPos;
+}
