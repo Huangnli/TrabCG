2775f5bc56aad7345a1f106858218c31bb1dd024
amandavc <amandavc27@gmail.com>
2020-12-03 19:57:40 -0400

    add shading shaders

--- OpenGLContext.h
+++ OpenGLContext.h
@@ -17,20 +17,29 @@ public:
 
     void initialize(); // Init our render
     void rendering() const; // Render Loop
+    void finalize() const;  // Finalize our render
+    
 private:
     unsigned int programId;
+    unsigned int programNone;
+    unsigned int programFlat;
+    unsigned int programSmooth;
+    unsigned int programPhong;
     unsigned int programAxis;
     unsigned int programLight;
     unsigned int vao;
     unsigned int vbo;
+    
     static void glutRenderCallback();                       // Render window
     static void glutReshapeCallback(int width, int height); // Reshape window
-    
-    void createShaderObjects();
-    void createShaderLight();
-    void createShaderAxis();
 
-    void finalize() const;  // Finalize our render
+    void createShadersNone();
+    void createShadersFlat();
+    void createShadersSmooth();
+    void createShadersPhong();
+    void createShadersLight();
+    void createShadersAxis();
+
 
     unsigned int loadAndCompileShader(const std::string &filename, const int glType) const;
     unsigned int linkShaderProgram(unsigned int vertexShaderId, unsigned int fragmentShaderId) const;
